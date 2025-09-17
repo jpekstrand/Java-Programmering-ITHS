2a4cb49cdb9ecb62944ca7944fcd77849eab8e5c
tomans <tom.anstensen@infotiv.se>
2025-09-17 11:20:00 +0200

    remove helloworld

--- HelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println();
-        System.out.println("Hej världen det regnar idag!");
-        System.out.println();
-    }
-}
