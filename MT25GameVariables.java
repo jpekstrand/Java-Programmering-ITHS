a45a4163b05fcb503724f4597a59cfca65757aca
tomans <tom.anstensen@infotiv.se>
2025-09-17 11:13:25 +0200

    Created new class gamevariables

---
+++ MT25GameVariables.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class MT25GameVariables{
+
+    
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String characterName;
+        String className;
+        int level = 0;
+        System.out.println();
+        characterName = "Conan";
+        className = "MJT";
+        System.out.println(characterName +" " + className +" ");
+
+
+
+
+
+    }
+    
+}
