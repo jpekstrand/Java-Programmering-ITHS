8896c79a088b8d79090f604abb8ebba6d6fa3323
tomans <tom.anstensen@infotiv.se>
2025-09-18 09:26:10 +0200

    Added scanner and level evaluation

--- MT25GameVariables.java
+++ MT25GameVariables.java
@@ -9,15 +9,20 @@ public class MT25GameVariables{
         String characterName;
         String className;
         int level = 0;
-        System.out.println();
-        characterName = "Conan";
-        className = "MJT";
-        System.out.println(characterName +" " + className +" ");
 
-
-
-
-
-    }
+        System.out.println("What is your name?");
+        characterName = scan.nextLine();
+        System.out.println("What is your class?");
+        className = scan.nextLine();
+        System.out.println("Your name is "+ characterName +"\nand your class is " + className);
+        System.out.println("Your level is: " + level);
     
+        if (level < 1){
+            System.out.println("You are not prepared! ...to adventure");
+        }
+        else {
+            System.out.println("Go out and have fun dear!");
+        }
+        scan.close();
+    }
 }
