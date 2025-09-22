2910570e08e22789c6aaa823ab7e8c3e5a7e6154
tomans <tom.anstensen@infotiv.se>
2025-09-22 12:53:40 +0200

    created method for horn evaluation

--- MakeUnicorns.java
+++ MakeUnicorns.java
@@ -2,21 +2,20 @@ public class MakeUnicorns {
     
     public static void main(String[] args) {
         Unicorn bengt = new Unicorn(5, "Blå", "Glad", true, true);
-        Unicorn ilse = new Unicorn(4, "Gul", "Likgiltig", true, true);
-/* 
-        System.out.println("This is Bengt: ");
-        System.out.println(bengt.getAge());
-        bengt.setAge(6);
-        System.out.println(bengt.getAge());
-*/
-        
-        System.out.println("Is this a unicorn? ");
+        Unicorn ilse = new Unicorn(4, "Gul", "Likgiltig", true, false);
 
-        if (ilse.horn == true){
-            System.out.println("Ilse is a unicorn");
+        isThisAUnicorn(ilse);
+        isThisAUnicorn(bengt);
+    }
+
+    static void isThisAUnicorn(Unicorn name) {
+            System.out.println("Is this a unicorn? ");
+
+        if (name.horn == true){
+            System.out.println(name + " is a unicorn");
         }
         else{
-            System.out.println("Ilse is an imposter! She is just a horse with a dream!");
+            System.out.println(name + " is an imposter! It's just a horse with a dream!");
         }
     }
 }
