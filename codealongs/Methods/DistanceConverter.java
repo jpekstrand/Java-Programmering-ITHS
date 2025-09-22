f0cfd2d57c366d1398fc38cceeb635c4c3f27499
tomans <tom.anstensen@infotiv.se>
2025-09-22 11:24:26 +0200

    added kilometersToMiles method and scanner for input

--- codealongs/Methods/DistanceConverter.java
+++ codealongs/Methods/DistanceConverter.java
@@ -1,16 +1,26 @@
 package codealongs.Methods;
 
+import java.util.Scanner;
+
 public class DistanceConverter {
     public static void main(String[] args) {
-        milesToKilometers(0.9);
-        milesToKilometers(54);
-        milesToKilometers(2789);
+        System.out.println("Please input a distance to convert (miles):");
+        Scanner scan = new Scanner(System.in);
+        double distance = scan.nextDouble();
+
+        milesToKilometers(distance);
+
     }
 
     static void milesToKilometers(double miles) {
-        
         double kilometers = miles * 1.60934;
         System.out.println(kilometers);
+    }
+
+    static void kilometersToMiles(double kilometers) {
+        double miles = kilometers / 1.60934;
+        System.out.println(miles);
+
     }
     
 }
